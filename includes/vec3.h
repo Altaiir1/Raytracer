42baef4668d9edef69e0fc5ba740d7bd6670e65a
Cargnx <117575681+Altaiir1@users.noreply.github.com>
2025-12-14 01:50:36 +0100

    [TASK] Add personal comments

--- includes/vec3.h
+++ includes/vec3.h
@@ -8,17 +8,22 @@ class vec3
 public:
     double e[3];
 
+    // Default: Creates (0, 0, 0) and sets custom values
     vec3() : e{0, 0, 0} {}
     vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
+    // Helper functions to get x, y, z values
     double x() const { return e[0]; }
     double y() const { return e[1]; }
     double z() const { return e[2]; }
 
+    // Making Math Work Naturally
+    // - Negation If v = (1, 2, 3), then -v = (-1, -2, -3)
     vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
-    double operator[](int i) const { return e[i]; }
-    double &operator[](int i) { return e[i]; }
+    double operator[](int i) const { return e[i]; }                 // Read-only
+    double &operator[](int i) { return e[i]; }                      // Can modify
 
+    // Compound Assignment (modify in place)
     vec3 &operator+=(const vec3 &v)
     {
         e[0] += v.e[0];
@@ -41,6 +46,7 @@ public:
         return *this *= 1 / t;
     }
 
+    // Length Calculation
     double length() const
     {
         return std::sqrt(length_squared());
